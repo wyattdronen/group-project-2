518ffad30f231dc0a86eb7d064af4464c180e148
cody-and <codya.0807@icloud.com>
2023-08-15 10:30:35 -0500

    added logout js in the public folder

--- public/js/login.js
+++ public/js/login.js
@@ -42,10 +42,6 @@ const loginFormHandler =async (event) => {
     }
   };
 
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
