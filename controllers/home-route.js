7c48ed55c41a7338335175a91975502298983b04
cody-and <codya.0807@icloud.com>
2023-08-16 13:58:52 -0500

    added home routes and started fixing server.js to render the handlebars

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,1 +1,57 @@
-const withAuth = require('../utils/auth');    // authentication middleware to ensure the user is logged in
+const withAuth = require('../utils/auth');    
+const router = require ('express').Router();
+const { User, Routine } = require('../models');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const user = await User.findByPk(userId, {
+      include: [{ model: Routine }],
+    });
+
+    res.render('home', { user }); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while fetching data.' });
+  }
+});
+router.get('/bmi', withAuth, (req, res) => {
+  res.render('bmi');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const user = await User.findByPk(userId);
+
+    res.render('profile', { user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while fetching data.' });
+  }
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/routine', withAuth, async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const user = await User.findByPk(userId, {
+      include: [{ model: Routine }],
+    });
+
+    res.render('routine', { user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while fetching data.' });
+  }
+});
+
+
+module.exports = router;
