5a3c8429a003364483ba12036de4458a9703285d
cody-and <codya.0807@icloud.com>
2023-08-17 14:26:25 -0500

    finished userRoute

--- controllers/api/routineRoutes.js
+++ controllers/api/routineRoutes.js
@@ -1,3 +1,3 @@
-const router = require('express').Router();
-const {Routine} = require('../../models');
-const withAuth = require('../../utils/auth')
+// const router = require('express').Router();
+// const {Routine} = require('../../models');
+// const withAuth = require('../../utils/auth')
