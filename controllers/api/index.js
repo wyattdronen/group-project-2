05a8a7ac01767ea80556945d43c6096e3d2169cb
cody-and <codya.0807@icloud.com>
2023-08-17 16:30:06 -0500

    finished routine routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const userDataRoutes = require('./userDataRoutes');
-// const routineRoutes = require('./routineRoutes');
+const routineRoutes = require('./routineRoutes');
 // const exerciseRoutes = require('./exerciseRoutes');
 
 router.use('/users', userRoutes);
 router.use('/userdata', userDataRoutes);
-// router.use('/routine', routineRoutes);
+router.use('/routine', routineRoutes);
 // router.use('/exercise', exerciseRoutes)
 
 module.exports = router;
--- controllers/api/routineRoutes.js
+++ controllers/api/routineRoutes.js
@@ -1,63 +1,38 @@
-// const router = require('express').Router();
-// const {Exercise, User, UserData, Routine } = require('../../models');
-// // const withAuth = require('../../utils/auth')
-// //get all routines
-// router.get('/', async (req, res) => {
-//     try {
-//         const routineData = await Routine.findAll({
-//             include: [
-//                 {model: User},
-//                 {model: UserData},
-//                 {model: Exercise}
-//             ]
-//         });
-//         res.status(200).json(routineData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-// // get one routine by id
-// router.get('/:id', async (req, res) => {
-//     try {
-//         const routineData = await Routine.findByPk(req.params.id, {
-//             include: [
-//                 {model: User},
-//                 {model: UserData},
-//                 {model: Exercise}
-//             ]
-//         });
+const router = require('express').Router();
+const { Routine } = require('../../models');
 
-//         if (!routineData) {
-//             res.status(404).json({ message: 'No routine found with this id!' });
-//             return;
-//         }
+router.get('/', async (req, res) => {
+  try {
+    const routines = await Routine.find();
+    res.json(routines);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching routines.' });
+  }
+});
 
-//         res.status(200).json(routineData);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-// // create a new routine
-// router.post('/', async (req, res) => {
-//     try {
-//         const newRoutine = await Routine.create(req.body);
+router.post('/', async (req, res) => {
+  try {
+    const newRoutine = await Routine.create(req.body);
+    res.status(201).json(newRoutine);
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid data. Routine could not be created.' });
+  }
+});
 
-//         res.status(200).json(newRoutine);
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
-// // update routine by id
-// router.put('/:id', async (req, res) => {
-//     await Routine.update(req.body, {
-//         where: {id: req.params.id }
-//     });
-//     res.status(200).json({ message: 'Routine updated!' });
-// });
-// // delete routine by id
-// router.delete('/:id', async (req, res) => {
-//     await Routine.destroy({
-//         where: {id: req.params.id}
-//     });
-//     res.status(200).json({message: 'Routine deleted successfully'});
-// });
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedRoutine = await Routine.findByIdAndDelete(id);
+
+    if (!deletedRoutine) {
+      return res.status(404).json({ error: 'Routine not found.' });
+    }
+
+    res.json({ message: 'Routine deleted successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while deleting the routine.' });
+  }
+});
+
+module.exports = router;
