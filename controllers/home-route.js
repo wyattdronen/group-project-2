82e1e93a3de38a4a367772c93f8867823a99288e
cody-and <codya.0807@icloud.com>
2023-08-17 20:51:12 -0500

    added login redirect still working on getting handlebars to dynamically display user info

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,5 +1,5 @@
 const withAuth = require('../utils/auth');    
-const router = require ('express').Router();
+const router = require('express').Router();
 const { User, UserData, Exercise, Routine } = require('../models');
 
 router.get('/', async (req, res) => {
@@ -12,31 +12,50 @@ router.get('/', async (req, res) => {
       ],
     });
 
-    res.render('home', { user }); 
+    res.render('home', { user, logged_in: !!userId }); 
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'An error occurred while fetching data.' });
   }
 });
-router.get('/bmi',withAuth, (req, res) => {
+
+router.get('/bmi', withAuth, (req, res) => {
   res.render('bmi');
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
   res.render('login');
 });
 
 router.get('/profile', withAuth, async (req, res) => {
   try {
     const userId = req.session.userId;
-    const user = await User.findByPk(userId);
+    const user = await User.findByPk(userId, {
+      include: [{ model: UserData }],
+    });
 
-    res.render('profile', { user });
+    if (!user) {
+      console.log('User not found'); // Add this line for debugging
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    console.log('User data:', user);
+
+    res.render('profile', {
+      name: user.name,
+      User_data: user.UserData,
+      logged_in: true,
+    });
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'An error occurred while fetching data.' });
+    console.error(err); // Log the error for debugging
+    res.status(500).json(err);
   }
 });
+
 
 router.get('/about', (req, res) => {
   res.render('about');
@@ -55,6 +74,5 @@ router.get('/routine', withAuth, async (req, res) => {
     res.status(500).json({ error: 'An error occurred while fetching data.' });
   }
 });
-
 
 module.exports = router;
